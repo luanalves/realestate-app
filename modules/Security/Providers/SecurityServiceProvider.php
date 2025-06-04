0820f04f164cbf319e3c52b5a2be6ef940150f30
Luan alves <luan_alves@hotmail.com>
2025-06-04 15:38:49 +0000

    PR by copilot

---
+++ modules/Security/Providers/SecurityServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author      Luan Silva
+ * @copyright   2025 The Dev Kitchen (https://www.thedevkitchen.com.br)
+ * @license     https://www.thedevkitchen.com.br  Copyright
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Security\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SecurityServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // Register Security module services here
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Bootstrap Security module services here
+    }
+}
