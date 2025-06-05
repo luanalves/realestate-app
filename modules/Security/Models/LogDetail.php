3caa1ed6f563399f17227e639e066858b1481e7e
Luan alves <luan_alves@hotmail.com>
2025-06-05 10:04:39 +0000

    update module Security to include security logging functionality

---
+++ modules/Security/Models/LogDetail.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @author      Luan Silva
+ * @copyright   2025 The Dev Kitchen (https://www.thedevkitchen.com.br)
+ * @license     https://www.thedevkitchen.com.br  Copyright
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Security\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model as MongoModel;
+
+class LogDetail extends MongoModel
+{
+    // ...existing code...
+    /**
+     * The collection associated with the model.
+     *
+     * @var string
+     */
+    protected $collection = 'log_details';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'security_log_id',
+        'details',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'security_log_id' => 'integer',
+        'details' => 'array',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // ...existing code...
+}
---
+++ modules/Security/Models/SecurityLog.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @author      Luan Silva
+ * @copyright   2025 The Dev Kitchen (https://www.thedevkitchen.com.br)
+ * @license     https://www.thedevkitchen.com.br  Copyright
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Security\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SecurityLog extends Model
+{
+    // ...existing code...
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'security_logs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'action',
+        'ip_address',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'user_id' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // ...existing code...
+}
