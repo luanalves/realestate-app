e3dc61b33daad19ee8cfd9611469fffcd704fb36
Luan alves <luan_alves@hotmail.com>
2025-06-15 06:43:16 -0300

    config(app): update domain to realestate.localhost

--- create_passport_client.php
+++ create_passport_client.php
@@ -7,11 +7,11 @@ $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
 // Create a password grant client if one doesn't exist
-if (! \Laravel\Passport\Client::where('password_client', 1)->exists()) {
-    $client = new \Laravel\Passport\Client;
+if (!Laravel\Passport\Client::where('password_client', 1)->exists()) {
+    $client = new Laravel\Passport\Client();
     $client->name = 'Password Grant Client';
-    $client->secret = \Illuminate\Support\Str::random(40);
-    $client->redirect = 'http://localhost';
+    $client->secret = Illuminate\Support\Str::random(40);
+    $client->redirect = 'http://realestate.localhost';
     $client->personal_access_client = false;
     $client->password_client = true;
     $client->revoked = false;
@@ -21,13 +21,13 @@ if (! \Laravel\Passport\Client::where('password_client', 1)->exists()) {
     echo 'Client ID: '.$client->id."\n";
     echo 'Client Secret: '.$client->plainSecret."\n";
 } else {
-    $client = \Laravel\Passport\Client::where('password_client', 1)->first();
+    $client = Laravel\Passport\Client::where('password_client', 1)->first();
     echo "Password grant client already exists!\n";
     echo 'Client ID: '.$client->id."\n";
     if ($client->secret) {
         echo "Client has a secret already set.\n";
     } else {
-        $client->secret = \Illuminate\Support\Str::random(40);
+        $client->secret = Illuminate\Support\Str::random(40);
         $client->save();
         echo 'New Client Secret: '.$client->plainSecret."\n";
     }
