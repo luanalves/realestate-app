e67756ddad94756b20eeca6efe577ad0d766347c
Luan alves <luan_alves@hotmail.com>
2025-06-19 21:06:06 -0300

    feat: adiciona SecurityServiceProvider no bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     Modules\UserManagement\Providers\UserManagementServiceProvider::class,
     Modules\RealEstate\Providers\RealEstateServiceProvider::class,
+    Modules\Security\Providers\SecurityServiceProvider::class,
     Laravel\Passport\PassportServiceProvider::class,
 ];
