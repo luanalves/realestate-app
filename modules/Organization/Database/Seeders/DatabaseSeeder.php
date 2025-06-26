7fdb1b1fe5c22ef93b6126586e3106979e7b6b63
Luan alves <luan_alves@hotmail.com>
2025-06-25 21:47:39 -0300

    feat: add Organization module seeders
    Added Organization module DatabaseSeeder that auto-discovers seeders. Created OrganizationDemoDataSeeder for generating test data with checks to prevent duplication.

---
+++ modules/Organization/Database/Seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @author      Luan Silva
+ * @copyright   2025 The Dev Kitchen (https://www.thedevkitchen.com.br)
+ * @license     https://www.thedevkitchen.com.br  Copyright
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Organization\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $seederPath = __DIR__;
+        $seederFiles = File::allFiles($seederPath);
+
+        foreach ($seederFiles as $file) {
+            $class = 'Modules\\Organization\\Database\\Seeders\\'.$file->getFilenameWithoutExtension();
+
+            if (
+                class_exists($class)
+                && $class !== self::class
+                && is_subclass_of($class, Seeder::class)
+            ) {
+                $this->call($class);
+            }
+        }
+    }
+}
