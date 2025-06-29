0f3cad35823b5c59956872f2098570ed6432cb76
Luan alves <luan_alves@hotmail.com>
2025-06-29 08:24:41 -0300

    feat(user): adicionar campos preferences e tenant_id
    - Adicionados campos preferences (JSON) e tenant_id ao model User
    - Criada migration para adicionar os novos campos na tabela users
    - Configurado cast automático para JSON no campo preferences
    - Implementada invalidação automática de cache nos eventos de update/delete
    - Preparado model para arquitetura multi-tenant

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable
         'email',
         'password',
         'role_id',
+        'preferences',
+        'tenant_id',
     ];
 
     /**
@@ -79,6 +81,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'preferences' => 'json',
         ];
     }
 
