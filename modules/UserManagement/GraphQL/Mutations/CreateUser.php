d8ab511dddd254c039a33a622682b2084986cfe9
Luan alves <luan_alves@hotmail.com>
2025-07-01 21:27:15 -0300

    fix(user): definir valor padrão para is_active no resolver CreateUser para evitar erro GraphQL non-null

--- modules/UserManagement/GraphQL/Mutations/CreateUser.php
+++ modules/UserManagement/GraphQL/Mutations/CreateUser.php
@@ -10,9 +10,9 @@ declare(strict_types=1);
 
 namespace Modules\UserManagement\GraphQL\Mutations;
 
-use Modules\UserManagement\Models\User;
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Support\Facades\Hash;
+use Modules\UserManagement\Models\User;
 use Modules\UserManagement\Services\UserManagementAuthorizationService;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
@@ -28,29 +28,29 @@ class CreateUser
     /**
      * Create a new user.
      *
-     * @param mixed $rootValue The result from the parent resolver
-     * @param array $args The arguments that were passed into the field
-     * @param GraphQLContext $context Arbitrary data that is shared between all fields of a single query
-     * @param ResolveInfo $resolveInfo Information about the query itself
-     * @return User
+     * @param mixed          $rootValue   The result from the parent resolver
+     * @param array          $args        The arguments that were passed into the field
+     * @param GraphQLContext $context     Arbitrary data that is shared between all fields of a single query
+     * @param ResolveInfo    $resolveInfo Information about the query itself
      */
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): User
     {
         $this->authService->authorizeUserManagementWrite();
-        
+
         $input = $args['input'];
-        
+
         // Hash the password
         $input['password'] = Hash::make($input['password']);
-        
+
         // Create the user
         $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
             'role_id' => $input['role_id'],
+            'is_active' => $input['is_active'] ?? true, // Use input value or default to true
         ]);
-        
+
         return $user;
     }
 }
